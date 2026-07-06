13c07116886d616c1d450d8cf172ada76a637b28
mptan47-dot <mptan47@gmail.com>
2026-07-06 21:17:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// MoonPlayScript JavaScript
-console.log("MoonPlayScript Loaded");
+const search = document.getElementById("search");
+const cards = document.querySelectorAll(".card");
+
+search.addEventListener("input", () => {
+  const value = search.value.toLowerCase();
+
+  cards.forEach(card => {
+    const name = card.dataset.name.toLowerCase();
+    card.style.display = name.includes(value) ? "block" : "none";
+  });
+});
